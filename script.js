4e9838be24ebf831b5d555b24bc606a3c169c63d
Santi <santilobos21@gmail.com>
2024-08-04 17:09:12 +0200

    desarrollo con javascript corregido

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-
-
 let icon = document.getElementById("themeIcon");
 
-function changeTheme() {
-    document.body.classList.toggle("lightTheme");
-    if (document.body.classList.contains("lightTheme")) {
+// Función para aplicar el tema guardado en localStorage
+function applySavedTheme() {
+    const savedTheme = localStorage.getItem('theme') || 'darkTheme';
+    document.body.classList.add(savedTheme);
+    if (savedTheme === 'lightTheme') {
         icon.textContent = "dark_mode";
     } else {
         icon.textContent = "light_mode";
     }
 }
+
+// Llama a la función para aplicar el tema guardado cuando se carga la página
+document.addEventListener('DOMContentLoaded', applySavedTheme);
+
+function changeTheme() {
+    document.body.classList.toggle("lightTheme");
+    if (document.body.classList.contains("lightTheme")) {
+        icon.textContent = "dark_mode";
+        localStorage.setItem('theme', 'lightTheme');
+    } else {
+        icon.textContent = "light_mode";
+        localStorage.setItem('theme', 'darkTheme');
+    }
+}
+
+
+
+
